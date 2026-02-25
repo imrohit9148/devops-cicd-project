f3eff543137f9c125b90cfc53ad289acf8370132
Rohit <imrohitpatil9148@gmail.com>
2026-02-25 17:46:19 +0530

    Docker and Jenkinfile

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "jenkins" {
+  ami           = "ami-0f5ee92e2d63afc18"
+  instance_type = "t2.micro"
+  key_name      = "your-key"
+}
+
+resource "aws_instance" "k8s" {
+  ami           = "ami-0f5ee92e2d63afc18"
+  instance_type = "t2.micro"
+  key_name      = "your-key"
+}
+
+resource "aws_ecr_repository" "repo" {
+  name = "cicd-app"
+}
